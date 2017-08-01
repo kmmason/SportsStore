5874fdced438aba3dd4746c0adadcc1cfbd50b01
Kevin Mason <kevin.mason@outlook.com>
2017-08-01 14:04:43 -0500

    SportsStore Creating the Navigation Controller

---
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        // GET: Nav
+        public string Menu()
+        {
+            return "This is NavController.";
+        }
+    }
+}
