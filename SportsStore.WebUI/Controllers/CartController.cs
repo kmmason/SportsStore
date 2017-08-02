77f200c66115fa720d4095d08c72337b8b1f7efc
Kevin Mason <kevin.mason@outlook.com>
2017-08-02 15:09:57 -0500

    SportsStore Displaying the Contents of the Cart

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -1,17 +1,33 @@
-﻿using System.Linq;
-using System.Web.Mvc;
-using SportsStore.Domain.Abstract;
+﻿using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Models;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace SportsStore.WebUI.Controllers
 {
     public class CartController : Controller
     {
         private IProductRepository repository;
-        public CartController(IProductRepository repo) { repository = repo; }
+
+        public CartController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
         public RedirectToRouteResult AddToCart(int productId, string returnUrl)
         {
-            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
             if (product != null)
             {
                 GetCart().AddItem(product, 1);
@@ -21,7 +37,8 @@ namespace SportsStore.WebUI.Controllers
 
         public RedirectToRouteResult RemoveFromCart(int productId, string returnUrl)
         {
-            Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+            Product product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
             if (product != null)
             {
                 GetCart().RemoveLine(product);
@@ -34,7 +51,8 @@ namespace SportsStore.WebUI.Controllers
             Cart cart = (Cart)Session["Cart"];
             if (cart == null)
             {
-                cart = new Cart(); Session["Cart"] = cart;
+                cart = new Cart();
+                Session["Cart"] = cart;
             }
             return cart;
         }
